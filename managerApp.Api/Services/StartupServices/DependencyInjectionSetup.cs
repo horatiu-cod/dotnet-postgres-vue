3ccf70e0008c46c4d3574cdc16bd8bb19df2a41b
horatiu-cod <53142289+horatiu-cod@users.noreply.github.com>
2023-01-24 16:17:51 +0000

    Add dotnet project

---
+++ managerApp.Api/Services/StartupServices/DependencyInjectionSetup.cs
@@ -0,0 +1,13 @@
+﻿namespace managerApp.Api.Services.StartupServices;
+
+public static class DependencyInjectionSetup
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddControllers(); 
+
+        return services;
+    }
+}
---
+++ managerApp.Api/Services/StartupServices/MapEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace managerApp.Api.Services.StartupServices;
+
+public static class MapEndpoints
+{
+    public static WebApplication MapEndpoint(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+}
